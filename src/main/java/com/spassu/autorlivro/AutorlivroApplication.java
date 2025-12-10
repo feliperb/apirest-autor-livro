c39beb67634acf682a8b3073321c681cf398985f
Felipe Ribeiro <feliperibeirobarros@gmail.com>
2025-12-10 10:04:31 -0300

    Iniciando projeto spring

---
+++ src/main/java/com/spassu/autorlivro/AutorlivroApplication.java
@@ -0,0 +1,13 @@
+package com.spassu.autorlivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorlivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorlivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spassu/autorlivro/AutorlivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spassu.autorlivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorlivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
